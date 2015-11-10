6a37558960f2ccd8b2bbec68b80a8de6be8fa49c
Team02-TeamGuinness <med6@rice.edu>
2015-11-10 11:24:23 -0600

    Attempting to get the code to write to Gcode

--- DoTheRobot.py
+++ DoTheRobot.py
@@ -15,3 +15,49 @@ extension = config.extension
 repetitions = config.repetitions
 
 #Define your global variables
+
+def currentXPos(x):
+
+	global x_currentPos
+	x_currentPos += x
+	return x_currentPos
+
+def guiGlobVars(kvarg):
+
+	#File
+	filename = kvarg['gcodeNameStr']
+	global f
+	f.close()
+	f = open(filename,'w')
+
+	#Flexion
+	global flexion
+	flexion = int(kvarg['flexion']
+
+	#Extension
+	global extension
+	extension = int(kvarg['extension']
+
+	#Repetitions
+	global repetitions 
+	repetitions = int(kvarg['repetitions']
+
+#Write your Gcode
+
+def writeGcode(kvarg):
+
+	if (kvarg): guiGlobVars(kvarg)
+	writefile.header(f)
+	f.writelines("G28\n")
+	f.writelines("G0 X" + str(flexion) + "\n")
+	writefile.closefile(f)
+
+def main():
+	
+	writeGcode()
+
+if __name__ == '__main__': main()
+
+
+
+	
